265042d5b73ef781c3680d83594c372671cfc97e
laoer536 <1547698569@qq.com>
2023-08-30 20:07:06 +0800

    build: prettier-plugin-tailwindcss

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,16 +1,16 @@
 export default function LoginPage() {
   return (
-    <div className={'bg-background w-full h-screen'}>
-      <div className={'w-screen-xl mx-auto flex'}>
-        <div className={'flex-1 pt-14 pl-24'}>
+    <div className={'h-screen w-full'}>
+      <div className={'w-screen-xl  mx-auto flex'}>
+        <div className={'flex-1 pl-24 pt-14'}>
           <div className={'flex items-center gap-7'}>
-            <img src="https://nextui.org/favicon.ico" alt="" className={'w-12 h-12'} />
+            <img src="https://nextui.org/favicon.ico" alt="" className={'h-12 w-12'} />
             <span></span>
           </div>
           <p></p>
           <img src="" alt="" />
         </div>
-        <div className={'flex-1 pt-14 pl-24'}></div>
+        <div className={'flex-1 pl-24 pt-14'}></div>
       </div>
     </div>
   )
