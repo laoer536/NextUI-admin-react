9a04e249ba1c87d5be88a32fbf35c7a348599b69
LJie <1547698569@qq.com>
2023-08-30 17:46:01 +0800

    refactor: login-page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,17 @@
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  return (
+    <div className={'bg-background w-full h-screen'}>
+      <div className={'w-screen-xl mx-auto flex'}>
+        <div className={'flex-1 pt-14 pl-24'}>
+          <div className={'flex items-center gap-7'}>
+            <img src="https://nextui.org/favicon.ico" alt="" className={'w-12 h-12'} />
+            <span></span>
+          </div>
+          <p></p>
+          <img src="" alt="" />
+        </div>
+        <div className={'flex-1 pt-14 pl-24'}></div>
+      </div>
+    </div>
+  )
 }
