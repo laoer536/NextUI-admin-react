0f533def1c0cf7c38e55f738a1291e4a2e6d18b2
LJie <1547698569@qq.com>
2023-08-30 16:44:02 +0800

    build: unplugin-auto-import add

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import Pages from 'vite-plugin-pages'
+import AutoImport from 'unplugin-auto-import/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -8,6 +9,13 @@ export default defineConfig({
     Pages({
       resolver: 'react',
       exclude: ['**/components/*'],
+    }),
+    AutoImport({
+      imports: ['react', 'react-router-dom'],
+      dts: './src/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true,
+      },
     }),
   ],
 })
