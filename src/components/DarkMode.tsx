6b713e3813003e15127a722ad91bfae667aa242f
laoer536 <1547698569@qq.com>
2023-08-30 20:49:28 +0800

    feat: dark mode add

---
+++ src/components/DarkMode.tsx
@@ -0,0 +1,36 @@
+type Themes = 'dark' | 'light'
+export default function DarkMode() {
+  const [curTheme, setCurTheme] = useState<Themes>('light')
+  const themeInit = () => {
+    setCurTheme(theme())
+    if (curTheme === 'light') {
+      document.documentElement.classList.remove('dark')
+    } else {
+      document.documentElement.classList.add('dark')
+    }
+    window.localStorage.setItem('theme', curTheme)
+  }
+
+  const handleToggleClick = () => {
+    const element = document.documentElement
+    element.classList.toggle('dark')
+    const isDark = element.classList.contains('dark')
+    localStorage.setItem('theme', isDark ? 'dark' : 'light')
+    setCurTheme(isDark ? 'dark' : 'light')
+  }
+  const theme = (): Themes => {
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return (localStorage.getItem('theme') || 'light') as Themes
+    }
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark'
+    }
+    return 'light'
+  }
+  useEffect(themeInit, [])
+  return (
+    <div>
+      <button onClick={handleToggleClick}>{curTheme} Mode</button>
+    </div>
+  )
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,12 @@
 import { useLocation } from 'react-router-dom'
+import DarkMode from '../../components/DarkMode.tsx'
 
 export default function IndexPage() {
   const location = useLocation()
   console.log('location', location)
-  return <div>IndexPage</div>
+  return (
+    <div>
+      <DarkMode /> IndexPage
+    </div>
+  )
 }
