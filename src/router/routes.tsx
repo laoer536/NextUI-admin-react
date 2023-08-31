b74b6ab768525e35434a2e50d4ebc0536cdd5155
laoer536 <1547698569@qq.com>
2023-08-31 21:34:36 +0800

    refactor: 404page

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,10 @@
 import type { RouteObject } from 'react-router-dom'
 import IndexPage from '@pages/index'
 import LoginPage from '@pages/login'
+import AboutPage from '@pages/about'
 export const routes: RouteObject[] = [
   { path: '/', element: <IndexPage /> },
   { path: 'login', element: <LoginPage /> },
+  { path: 'about', element: <AboutPage /> },
+  { path: '*', element: <div>404</div> },
 ]
