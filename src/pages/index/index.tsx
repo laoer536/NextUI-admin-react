318972581c89a640a8a3fef7ff27dc0cfe0bf7c8
laoer536 <1547698569@qq.com>
2023-09-12 19:49:09 +0800

    style: code delete

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,7 @@
-import { useLocation, useMatches } from 'react-router-dom'
 import AdminBase from '@layouts/AdminBase.tsx'
 
 export default function IndexPage() {
-  const location = useLocation()
   const Outlet = useOutlet() // Outlet=page
-  console.log('location', location, useMatches())
   return (
     <div>
       <AdminBase>{Outlet}</AdminBase>
